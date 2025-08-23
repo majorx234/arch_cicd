6020b8c77db02322d91756159ef4d9582875a20c
MajorX234 <majorx234@googlemail.com>
2025-08-24 00:02:28 +0200

    Remove datpicker and fields wich should uneditable from create form

--- cd_manager/forms.py
+++ cd_manager/forms.py
@@ -1,20 +1,8 @@
-from django.forms import ModelForm, DateField
+from django.forms import ModelForm
 from cd_manager.models import Package
-from bootstrap_datepicker_plus.widgets import DatePickerInput
 
 
 class PackageForm(ModelForm):
-    # not needed just for testing
-    build_date = DateField(input_formats=["%Y-%m-%d"], label="build_date",
-                           widget=DatePickerInput(
-                               options={
-                                   "format": "YYYY-MM-DD",
-                                   "showClose": True,
-                                   "showClear": True,
-                                   "showTodayButton": True,
-                                }
-                              ))
-
     class Meta:
         model = Package
         fields = (
@@ -22,9 +10,6 @@ class PackageForm(ModelForm):
             'repo_url',
             'makepkg_extra_args',
             'clean_build',
-            'build_status',
-            'build_date',
-            'build_output',
             'repo_push',
             'repo_release_push',
         )
